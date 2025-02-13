63da78b53326c5b7b0dd19befc3ce5b0a6706ff4
quynh2003 <quynh2003@imail.edu.vn>
2025-02-13 11:28:38 +0700

    add role to user

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
     has_secure_password
     validates :email, :username, uniqueness: true
     has_many :events
+    enum role: { user: 0, admin: 1 }
     def as_json(opts = {})
-        super(opts.merge(only: [:id, :username, :email, :name]))
+        super(opts.merge(only: [:id, :username, :email, :name, :role]))
     end
 end
