fd06bab160794992033a0f33f34c31254a08c35e
trdung <trungdungg@gmail.com>
2025-02-14 05:40:32 +0000

    fix user model

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -2,8 +2,9 @@ class User < ApplicationRecord
     has_secure_password
     validates :email, :username, uniqueness: true
     has_many :events
-    enum role: { user: 0, admin: 1 }
+    enum :role, { user: 0, admin: 1 }
     def as_json(opts = {})
         super(opts.merge(only: [:id, :username, :email, :name, :role]))
     end
 end
+
