696015de1890d46a50a673e24422a5932b36ed2e
quynh2003 <quynh2003@imail.edu.vn>
2025-02-15 18:22:18 +0700

    fix migrate

--- backend/db/migrate/20250215095927_change_checkin_checkout_time_to_datetime.rb
+++ backend/db/migrate/20250215095927_change_checkin_checkout_time_to_datetime.rb
@@ -1,6 +1,6 @@
 class ChangeCheckinCheckoutTimeToDatetime < ActiveRecord::Migration[8.0]
   def change
-    change_column :hotels, :checkin_time, :datetime, using: 'checkin_time AT TIME ZONE \'UTC\' AT TIME ZONE \'+07:00\''
-    change_column :hotels, :checkout_time, :datetime, using: 'checkin_time AT TIME ZONE \'UTC\' AT TIME ZONE \'+07:00\''
+    change_column :hotels, :checkin_time, :datetime
+    change_column :hotels, :checkout_time, :datetime
   end
 end
