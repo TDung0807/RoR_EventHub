50d0a7ec6222f5d375d89f2aef113cf3bc1b2997
quynh2003 <quynh2003@imail.edu.vn>
2025-02-15 18:00:12 +0700

    hot fix model

--- backend/db/migrate/20250215100033_change_distance_and_contact_to_string.rb
+++ backend/db/migrate/20250215100033_change_distance_and_contact_to_string.rb
@@ -1,4 +1,6 @@
 class ChangeDistanceAndContactToString < ActiveRecord::Migration[8.0]
   def change
+    change_column :your_table_name, :distance, :string
+    change_column :your_table_name, :contact, :string
   end
 end
