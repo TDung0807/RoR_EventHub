9d6bea1b67e3b26a15b30d147653597d88e26d07
Trdung <trungdungg0807@gmail.com>
2025-02-16 22:06:55 +0700

    asdfasdf'

--- backend/db/migrate/20250216112842_change_duration_to_float_in_events.rb
+++ backend/db/migrate/20250216112842_change_duration_to_float_in_events.rb
@@ -1,5 +1,5 @@
 class ChangeDurationToFloatInEvents < ActiveRecord::Migration[7.0]
   def change
-    change_column :events, :duration, 'float USING duration::float'
+    change_column :events, :duration, 'float USING (EXTRACT(EPOCH FROM duration))'
   end
 end
