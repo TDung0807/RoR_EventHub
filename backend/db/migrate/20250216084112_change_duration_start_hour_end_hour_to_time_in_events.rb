d384a930314542340997874bc9fc55c13195b4b9
Trdung <trungdungg0807@gmail.com>
2025-02-16 17:59:36 +0700

    hot fix

--- backend/db/migrate/20250216084112_change_duration_start_hour_end_hour_to_time_in_events.rb
+++ backend/db/migrate/20250216084112_change_duration_start_hour_end_hour_to_time_in_events.rb
@@ -1,6 +1,5 @@
 class ChangeDurationStartHourEndHourToTimeInEvents < ActiveRecord::Migration[8.0]
   def change
-    change_column :events, :duration, :time, using: 'to_timestamp(duration) AT TIME ZONE \'UTC\''
     change_column :events, :startHour, :time, using: 'to_timestamp("startHour") AT TIME ZONE \'UTC\''
     change_column :events, :endHour, :time, using: 'to_timestamp("endHour") AT TIME ZONE \'UTC\''
   end
--- backend/db/migrate/20250216103456_change_duration_to_float_in_events.rb
+++
@@ -1,5 +0,0 @@
-class ChangeDurationToFloatInEvents < ActiveRecord::Migration[8.0]
-  def change
-    change_column :events, :duration, :float
-  end
-end
