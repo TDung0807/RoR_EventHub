90062dc20919b66b6798299ab05a2fc69c202aad
Trdung <trungdungg0807@gmail.com>
2025-02-16 14:51:47 +0700

    change address t location

--- backend/app/models/event.rb
+++ backend/app/models/event.rb
@@ -3,7 +3,7 @@ class Event < ApplicationRecord
     validates :date, presence: true
     validates :description, presence: false
     validates :duration, numericality: { greater_than: 0 }, allow_nil: true
-    validates :address, presence: true
+    validates :location, presence: true
     validates :participants, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, allow_nil: true
     validates :startAt, numericality: { greater_than_or_equal_to: 0 }, allow_nil: true
     belongs_to :user
