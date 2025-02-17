fd6eceb5a1d47a00ee36101321dc8dddce13d3bc
TrDung <52100783@student.tdtu.edu.vn>
2025-02-17 14:17:06 +0700

    email notify login

--- backend/app/controllers/quests_controller.rb
+++ backend/app/controllers/quests_controller.rb
@@ -12,12 +12,14 @@ class QuestsController < ApplicationController
 
   def create
     @quest = Quest.find_by(email: quest_params[:email])
-
+  
     if @quest
       if params[:group_ids]
-        @quest.groups = Group.find(params[:group_ids])  
+        @quest.groups = Group.find(params[:group_ids])
       end
       if @quest.update(quest_params)
+        # Send email notification if the quest is added to a group
+        NotifierMailer.group_assignment_notification(@quest).deliver_later
         render json: { message: "Quest updated successfully", quest: @quest.as_json }, status: :ok
       else
         render json: { message: "Update failed", errors: @quest.errors.full_messages }, status: :unprocessable_entity
@@ -26,14 +28,17 @@ class QuestsController < ApplicationController
       @quest = Quest.new(quest_params)
       if @quest.save
         if params[:group_ids]
-          @quest.groups = Group.find(params[:group_ids]) 
+          @quest.groups = Group.find(params[:group_ids])
         end
+        # Send email notification if the quest is added to a group
+        NotifierMailer.group_assignment_notification(@quest).deliver_later
         render json: @quest.as_json, status: :ok
       else
         render json: { message: "Creation error", error: @quest.errors.full_messages }, status: :bad_request
       end
     end
   end
+  
 
   def index
     quests = Quest.all
@@ -52,21 +57,24 @@ class QuestsController < ApplicationController
 
   def update
     @quest = Quest.find_by(id: params[:id])
-
+  
     unless @quest
       render json: { message: "Quest not found" }, status: :not_found
       return
     end
-
+  
     if @quest.update(quest_params)
       if params[:group_ids]
-        @quest.groups = Group.find(params[:group_ids]) 
+        @quest.groups = Group.find(params[:group_ids])
       end
+      # Send email notification if the quest is added to a group
+      NotifierMailer.group_assignment_notification(@quest).deliver_later
       render json: { message: "Updated successfully", quest: @quest.as_json }, status: :ok
     else
       render json: { message: "Update failed", errors: @quest.errors.full_messages }, status: :unprocessable_entity
     end
   end
+  
 
   def destroy
     @quest = Quest.find_by(id: params[:id])
