0ed91fbd67850809362bfd354c8a31d6f0b7c3cd
TrDung <52100783@student.tdtu.edu.vn>
2025-02-17 14:17:49 +0700

    add notification service

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_12_041527) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_12_100514) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -97,6 +97,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_12_041527) do
     t.float "checkin_time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "distance"
+    t.string "contact"
   end
 
   create_table "ingredients", force: :cascade do |t|
