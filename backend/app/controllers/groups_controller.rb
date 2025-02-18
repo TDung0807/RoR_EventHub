b1d1fe3dfe1374c72acc560e72c21e6b564204da
trdung <trungdungg@gmail.com>
2025-02-18 07:41:42 +0000

    update params

--- backend/app/controllers/groups_controller.rb
+++ backend/app/controllers/groups_controller.rb
@@ -3,7 +3,7 @@ class GroupsController < ApplicationController
   
     private
     def group_params
-      params.require(:group).permit(:group, :groupStatus, :quantity, :description, :hotel_remark, :transport_remark, :dish_remark, :hotel_id, dish_ids: [])
+      params.require(:group).permit(:group, :groupStatus, :transport_id, :quantity, :description, :hotel_remark, :transport_remark, :dish_remark, :hotel_id, dish_ids: [])
     end
   
     public
