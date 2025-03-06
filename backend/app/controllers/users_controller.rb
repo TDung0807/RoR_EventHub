a740045620f27611cbb5473ceedb1087fb645d43
TrDung <52100783@student.tdtu.edu.vn>
2025-03-06 16:02:52 +0700

    fix create user

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -2,14 +2,20 @@ class UsersController < ApplicationController
   skip_before_action :authenticate, only: [:create, :index, :show]
 
   def create
-    @user = User.new(user_params)
-    if @user.save
-      NotifierMailer.welcome_email(@user, @user.password).deliver_later
+    @user = User.find_by(email: user_params[:email])
+    if @user
       render json: @user.as_json, status: :ok
     else
-      render json: { message: "Error created", errors: @user.errors.full_messages }, status: :bad_request
+      @user = User.new(user_params)
+      if @user.save
+        NotifierMailer.welcome_email(@user, @user.password).deliver_later
+        render json: @user.as_json, status: :ok
+      else
+        render json: { message: "Error created", errors: @user.errors.full_messages }, status: :bad_request
+      end
     end
   end
+  
 
   def index
     render json: { users: User.all.map(&:as_json) }
